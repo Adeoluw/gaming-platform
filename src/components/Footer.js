6f4b98f223bc21e5f5435ed5863dd92454a3c5f7
Adeoluw <104023937+Adeoluw@users.noreply.github.com>
2024-11-11 02:39:34 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,13 @@ import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 
-
-
 export default function Footer() {
-
-
-
   return (
     <>
       <footer>
         <div className="footer-logo-section">
           <h1 className="footer-title-head">LOGO</h1>
-          <p>
+          <p className="footer-middle">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.
           </p>
@@ -23,26 +18,36 @@ export default function Footer() {
         </div>
         <div className="footer-about">
           <ul>
-            <li className="footer-about-head footer-title-head" >About Us</li>
-            <li>Zeux</li>
-            <li>Portfolio</li>
-            <li>Carrers</li>
+            <li className="footer-about-head footer-title-head">About Us</li>
+            <div className="footer-middle footer-li-body">
+              <li>Zeux</li>
+              <li>Portfolio</li>
+              <li>Carrers</li>
+            </div>
             <li>contact us</li>
           </ul>
         </div>
         <div className="contact-footer">
           <p className="footer-title-head">contact us</p>
-          <p>
+          <p className="footer-middle">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.
           </p>
           <p>+908 89097 890</p>
         </div>
-              <div className="socials-footer">
-                  <div className="social"><FaFacebook /></div>
-                  <div className="social"><FaInstagram /></div>
-                  <div className="social"><FaXTwitter /></div>
-                  <div className="social"><FaLinkedin /></div>
+        <div className="socials-footer">
+          <div className="social">
+            <FaFacebook />
+          </div>
+          <div className="social">
+            <FaInstagram />
+          </div>
+          <div className="social">
+            <FaXTwitter />
+          </div>
+          <div className="social">
+            <FaLinkedin />
+          </div>
         </div>
       </footer>
     </>
